47b4b7fd6e8b2651ebbe0d39a9151a612ecf7ec2
boredSquidward <120290053+boredSquidward@users.noreply.github.com>
2023-07-06 15:52:52 -0400

    img loader and html-webpack-plugin add

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import "./style.css";
+import img from "./assets/img.jpeg";
 
 const greet = () => {
-  const root = document.querySelector(".root");
+  const [root] = document.getElementsByTagName("body");
+
+  const myImg = new Image();
+  myImg.src = img;
 
   root.innerHTML = "<h1>Welcome to Webpack Tutorials</h1>";
+  root.appendChild(myImg);
 };
 
 greet();
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    filename: "bundle.[contenthash].js",
     clean: true,
   },
   module: {
@@ -13,6 +14,11 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(jpe?g|png|gif|webp|svg)$/i,
+        type: "asset/resource",
+      },
     ],
   },
+  plugins: [new HtmlWebpackPlugin()],
 };
