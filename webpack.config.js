0354bda36536d1bc55cbc4b3b0c6d567383d2c41
boredSquidward <120290053+boredSquidward@users.noreply.github.com>
2023-07-06 14:10:57 -0400

    setting up the basics

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require("path");
 
 module.exports = {
-  mode: process.env.MODE === "prod" ? "production" : "development",
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
